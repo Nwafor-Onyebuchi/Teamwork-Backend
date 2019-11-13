40128d492ebe10b219a9b5ac978279a4f7fcfbe6
Onyebuchi-Nwafor <buchilazarus4@gmail.com>
2019-11-13 11:09:20 +0100

    Create route for updating user profile (Put route)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -55,5 +55,22 @@ const router = (app) => {
       }
     );
   });
+
+  // Update an existing user
+  app.put("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { first_name, email } = req.body;
+
+    pool.query(
+      "UPDATE user_profile SET first_name = $1, email = $2 WHERE id = $3",
+      [first_name, email, id],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).send(`User modified with ID: ${id}`);
+      }
+    );
+  });
 };
 module.exports = router;
